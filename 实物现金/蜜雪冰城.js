5a2842afa09782543cef5eb1c484c153b6c6990c
是大叔啊~ <65339736+KangCheng-H@users.noreply.github.com>
2025-03-25 14:23:35 +0800

    Update 蜜雪冰城.js

--- 实物现金/蜜雪冰城.js
+++ 实物现金/蜜雪冰城.js
@@ -283,7 +283,7 @@ async function SendMsg(message) {
     if (!message) return;
     if (Notify > 0) {
         if ($.isNode()) {
-            var notify = require("./sendNotify");
+            var notify = require("../sendNotify");
             await notify.sendNotify($.name, message)
         } else {
             $.msg($.name, '', message)
